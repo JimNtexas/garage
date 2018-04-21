abdf3db7147987cbc4a90ee4c20267d070814708
JimNtexas <grayraven42@gmail.com>
2018-04-21 13:41:08 -0500

    mqtt troubles

--- Android/garage/app/src/main/java/com/grayraven/garage/MainActivity.java
+++ Android/garage/app/src/main/java/com/grayraven/garage/MainActivity.java
@@ -1,13 +1,69 @@
 package com.grayraven.garage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG = "Garage_Main";
+    private static Context mContext = GarageApp.getAppContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this.getApplicationContext();
         setContentView(R.layout.activity_main);
     }
+
+    final String broker = "10.211.1.127:1883";
+    final String password = "monkey123";
+    final String subscriptionTopic = "garage_door";
+
+
+    MqttConnectOptions options = new MqttConnectOptions();
+    //options.setUserName("pi"); //cannot resolve setUserName!!!
+    //options.setPassword(password.toCharArray()); //cannot resolve setPassword!!!!
+
+
+    String clientId = MqttClient.generateClientId();
+    MqttAndroidClient client = new MqttAndroidClient(mContext, broker,
+                    clientId);
+
+    IMqttToken token;
+    {
+        try {
+            token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.d(TAG, "onSuccess");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d(TAG, "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
+
+
