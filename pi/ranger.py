b146da31394928444242ba90d0806735736a9a43
JimNtexas <grayraven42@gmail.com>
2018-04-28 20:22:52 -0500

    changed output to feet, tested with working ultrasonic device

--- pi/ranger.py
+++ pi/ranger.py
@@ -23,49 +23,53 @@ GPIO.setup(GPIO_ECHO, GPIO.IN)
 
 
 def sendDistance(dist):
-    msg = "distance: " + str(dist)
-    print("mqtt publishes: " + msg)
-    publish.single(MQTT_PATH, msg, hostname=MQTT_SERVER)
-    print ('Published:' + msg)
+	dist = dist * 0.3937
+	msg = "distance: " + str(dist) + " inches"
+	print("mqtt publishes: " + msg)
+	publish.single(MQTT_PATH, msg, hostname=MQTT_SERVER)
+	print ('Published:' + msg)
 
 def distance():
-    print("distance running")
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
+	GPIO.output(GPIO_TRIGGER, False)
+	time.sleep(0.02)
+	print("distance running")
+	# set Trigger to HIGH
+	GPIO.output(GPIO_TRIGGER, True)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	GPIO.output(GPIO_TRIGGER, False)
 
-    StartTime = time.time()
-    StopTime = time.time()
+	StartTime = time.time()
+	StopTime = time.time()
 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
+	# save StartTime
+	while GPIO.input(GPIO_ECHO) == 0:
+		StartTime = time.time()
 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
+	# save time of arrival
+	while GPIO.input(GPIO_ECHO) == 1:
+		StopTime = time.time()
 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
+	# time difference between start and arrival
+	TimeElapsed = StopTime - StartTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	distance = (TimeElapsed * 34300) / 2
+	
 
-    return distance
+	return distance
 
 
 if __name__ == '__main__':
-    try:
-        while True:
-            dist = 3 #distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            sendDistance(dist)
-            time.sleep(2)
+	try:
+		while True:
+			dist = distance()
+			#print ("Measured Distance = %.1f cm" % dist)
+			sendDistance(dist)
+			time.sleep(2)
 
-    # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+	# Reset by pressing CTRL + C
+	except KeyboardInterrupt:
+		print("Measurement stopped by User")
+		GPIO.cleanup()
