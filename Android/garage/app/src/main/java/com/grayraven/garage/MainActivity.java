a3bc15fd848c5dcf0d48f903d0b0fb503817c4ef
JimNtexas <grayraven42@gmail.com>
2018-04-28 21:47:24 -0500

    Fixed an issue with mqtt options and set background color of app

--- Android/garage/app/src/main/java/com/grayraven/garage/MainActivity.java
+++ Android/garage/app/src/main/java/com/grayraven/garage/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
@@ -17,29 +20,47 @@ public class MainActivity extends AppCompatActivity {
 
     final String TAG = "Garage_Main";
     private static Context mContext = GarageApp.getAppContext();
+    private TextView main_text;
+    View main_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = this.getApplicationContext();
         setContentView(R.layout.activity_main);
+        main_text = (TextView)findViewById(R.id.main_text);
+        main_text.setText("waiting for message");
+        main_layout = (View)findViewById(R.id.main_layout);
+        main_layout.setBackgroundColor(getResources().getColor(R.color.yellow));
     }
 
     final String broker = "tcp://10.211.1.127";
-    final String password = "monkey123";
+    String password = "monkey123";
     String username = "pi";
-    final String topic = "test_event";
+    final String topic = "door_distance";
+
+
 
     MqttClient client;
     IMqttToken token;
+
+
+
+
+
     String clientId = MqttAsyncClient.generateClientId();
     {
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setPassword(password.toCharArray());
+        connectOptions.setUserName(username);
         try {
+
+
             MemoryPersistence persistence = new MemoryPersistence();
             client = new MqttClient(broker, clientId, persistence);
             client.setCallback(new MqttEventCallback());
             client.setTimeToWait(3000);
-            client.connect();
+            client.connect(/*connectOptions*/);
             client.subscribe(topic);
         } catch (MqttSecurityException e) {
             e.printStackTrace();
